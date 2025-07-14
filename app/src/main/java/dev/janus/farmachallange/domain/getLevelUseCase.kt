edbeeafc338f9e261e7e1bc23b8a9cd0e2223047
Obed Salgado <obed.sabian@gmail.com>
2025-07-14 01:57:53 -0600

    refactorizacion y rediseño

--- app/src/main/java/dev/janus/farmachallange/domain/getLevelUseCase.kt
+++ app/src/main/java/dev/janus/farmachallange/domain/getLevelUseCase.kt
@@ -4,7 +4,7 @@ import dev.janus.farmachallange.data.model.Nivel
 import dev.janus.farmachallange.data.network.RepoPregunta
 import javax.inject.Inject
 
-class getLevelUseCase @Inject constructor(private val repoPregunta: RepoPregunta) {
+class GetLevelUseCase @Inject constructor(private val repoPregunta: RepoPregunta) {
     suspend operator fun invoke(): List<Nivel> = repoPregunta.getLevelName()
 
 }
--- app/src/main/java/dev/janus/farmachallange/domain/getQuestionDataUseCase.kt
+++ app/src/main/java/dev/janus/farmachallange/domain/getQuestionDataUseCase.kt
@@ -4,8 +4,8 @@ import dev.janus.farmachallange.data.model.Pregunta
 import dev.janus.farmachallange.data.network.RepoPregunta
 import javax.inject.Inject
 
-class getQuestionDataUseCase @Inject constructor(private val repoPregunta: RepoPregunta) {
+class GetQuestionDataUseCase @Inject constructor(private val repoPregunta: RepoPregunta) {
 
-    suspend operator fun invoke(idNivel: String, idRonda: String): List<Pregunta?> =
+    suspend operator fun invoke(idNivel: String, idRonda: String): List<Pregunta> =
         repoPregunta.getQuestionsData(idNivel, idRonda)
 }
